2815b9f77deae73fa06988d15273ddf0078bf734
ReginYuan <2496943860@qq.com>
2025-03-31 15:27:51 +0800

    feat: 修改代码

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,18 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    browser: true // 新增浏览器环境支持
+    browser: true,
+    es2021: true
   },
   extends: ['plugin:vue/vue3-essential', '@vue/eslint-config-standard', '@vue/prettier'],
   parserOptions: {
-    parser: '@babel/eslint-parser', // 更新解析器
-    requireConfigFile: false, // 新增：不需要 babel 配置文件
-    sourceType: 'module', // 新增：使用 ES 模块
-    ecmaVersion: 2020 // 新增：支持 ES2020
+    ecmaVersion: 'latest',
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
