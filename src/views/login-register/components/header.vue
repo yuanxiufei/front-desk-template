0400644bcd9cc56f84bf34c6956abc9ec1a54df1
ReginYuan <2496943860@qq.com>
2025-04-14 22:21:44 +0800

    feat: 修改按钮

--- src/views/login-register/components/header.vue
+++ src/views/login-register/components/header.vue
@@ -4,18 +4,13 @@
     <img
       v-lazy
       class="m-auto"
-      src="https://res.lgdsunday.club/signlogo.png"
+      src="https://m.imooc.com/static/wap/static/common/img/logo-small@2x.png"
       alt=""
     />
   </div>
   <!-- 头部图标：移动端 -->
   <div class="h-[111px] xl:hidden">
-    <img
-      v-lazy
-      class="dark:hidden"
-      src="https://res.lgdsunday.club/login-bg.png"
-      alt=""
-    />
+    <img v-lazy class="dark:hidden" src="https://res.lgdsunday.club/login-bg.png" alt="" />
     <img
       v-lazy
       class="h-5 absolute top-[5%] left-[50%] translate-x-[-50%]"
