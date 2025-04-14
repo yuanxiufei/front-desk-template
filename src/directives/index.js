da0c88b6397e89ca7cf8c88c90a5bb07e401a4bf
ReginYuan <2496943860@qq.com>
2025-04-14 22:16:34 +0800

    feat: 修改按钮

--- src/directives/index.js
+++ src/directives/index.js
@@ -5,7 +5,7 @@ export default {
   async install(app) {
     // https://cn.vitejs.dev/guide/features.html#glob-import
     // import.meta.globEager 为同步导入
-    const directives = import.meta.globEager('./modules/*.js')
+    const directives = import.meta.glob('./modules/*.js', { eager: true })
     for (const [key, value] of Object.entries(directives)) {
       // 拼接组件注册的 name
       const arr = key.split('/')
